9db83f8fffba8b47202ad8179ab3a7662997ec74
bloatedfish <52055420+bloatedfish@users.noreply.github.com>
2021-12-23 04:56:18 -0600

    drawing chess pieces

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import webbrowser
 from PySide6 import QtCore, QtWidgets, QtGui
 from PySide6.QtWidgets import QMenuBar, QMenu
-from PySide6.QtGui import QPainter, QBrush, QPen
+from PySide6.QtGui import QPainter, QBrush, QPen, QPixmap
 from PySide6.QtCore import Qt
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
@@ -40,6 +40,7 @@ class MyWidget(QtWidgets.QWidget):
         brush2 = QBrush(Qt.darkGray, Qt.SolidPattern)
         for x in range(0, 8):
             for y in range(0, 8):
+                # Draw Grid
                 if((x+y)%2 == 0):
                     painter.setPen(pen)
                     painter.setBrush(brush)
@@ -48,6 +49,47 @@ class MyWidget(QtWidgets.QWidget):
                     painter.setPen(pen2)
                     painter.setBrush(brush2)
                     painter.drawRect(x*80, y*80+40, 80, 80)
+
+                # Draw Chess Pieces    
+                if(y == 0):
+                    if(x == 0 or x == 7):
+                        pixmap = QPixmap('art/rookB.png')
+                        painter.drawPixmap(x*80, y*80+40, 80, 80, pixmap)
+                    elif(x == 1 or x == 6):
+                        pixmap = QPixmap('art/knightB.png')
+                        painter.drawPixmap(x*80, y*80+40, 80, 80, pixmap)
+                    elif(x == 2 or x == 5):
+                        pixmap = QPixmap('art/bishopB.png')
+                        painter.drawPixmap(x*80, y*80+40, 80, 80, pixmap)
+                    elif(x == 3):
+                        pixmap = QPixmap('art/queenB.png')
+                        painter.drawPixmap(x*80, y*80+40, 80, 80, pixmap)
+                    else:
+                        pixmap = QPixmap('art/kingB.png')
+                        painter.drawPixmap(x*80, y*80+40, 80, 80, pixmap)
+                elif(y == 1):
+                    pixmap = QPixmap('art/pawnB.png')
+                    painter.drawPixmap(x*80, y*80+40, 80, 80, pixmap)
+                elif(y == 6):
+                    pixmap = QPixmap('art/pawnW.png')
+                    painter.drawPixmap(x*80, y*80+40, 80, 80, pixmap)
+                elif(y == 7):
+                    if(x == 0 or x == 7):
+                        pixmap = QPixmap('art/rookW.png')
+                        painter.drawPixmap(x*80, y*80+40, 80, 80, pixmap)
+                    elif(x == 1 or x == 6):
+                        pixmap = QPixmap('art/knightW.png')
+                        painter.drawPixmap(x*80, y*80+40, 80, 80, pixmap)
+                    elif(x == 2 or x == 5):
+                        pixmap = QPixmap('art/bishopW.png')
+                        painter.drawPixmap(x*80, y*80+40, 80, 80, pixmap)
+                    elif(x == 3):
+                        pixmap = QPixmap('art/queenW.png')
+                        painter.drawPixmap(x*80, y*80+40, 80, 80, pixmap)
+                    else:
+                        pixmap = QPixmap('art/kingW.png')
+                        painter.drawPixmap(x*80, y*80+40, 80, 80, pixmap)
+        
         painter.end()
 
 if __name__ == "__main__":
